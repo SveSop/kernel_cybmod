2d778a346797b3dbcf132a42eec6e3e3cfb76b7d
Sveinar Søpler <cybermax@dexter.no>
2019-03-30 16:33:51 +0100

    Update to kernel 5.0.5 + add block patches

--- blk-patches/blk_patch.sh
+++ blk-patches/blk_patch.sh
@@ -2,18 +2,13 @@
 RED='\033[1;31m'
 NC='\033[0m'
 DIR=$(dirname $0)
-for each in $DIR/0*.patch
-do
-        echo -e "${RED}***************"
-        echo -e "Applying patch: $(basename $each)"
-        echo -e "***************${NC}"
-	patch -p1 < $each
-done
-for each in $DIR/blk-patches/0*.patch
+
+for each in $DIR/00*.patch
 do
         echo -e "${RED}***************"
         echo -e "Applying patch: $(basename $each)"
         echo -e "***************${NC}"
         patch -p1 < $each
 done
+
 echo Done patching
